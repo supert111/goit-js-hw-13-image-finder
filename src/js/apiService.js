cdc5bce8f0b874cf3e67f9b12363d181ca02a215
supert111 <sportsmen@bigmir.net>
2021-03-13 22:54:45 +0200

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,6 +1,7 @@
 import galleryTemplate from '../templates/gallery-template.hbs';
 
 var debounce = require('lodash.debounce');
+const key = '20667930-64a6ab52d11330f7fc72003b0';
 
 const inputFormRef = document.querySelector('.search-form');
 const inputName = inputFormRef.elements.query;
@@ -14,7 +15,9 @@ console.log(searchKeyWord);
 // const search
 
 
-//function fetch
+async function fetch (searchKey) {
+    const searchResult = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchKey}&page=номер_страницы&per_page=12&key=${key}`)
+}
 
-//https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+//https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchKey}&page=номер_страницы&per_page=12&key=твой_ключ
 // key: 20667930-64a6ab52d11330f7fc72003b0
