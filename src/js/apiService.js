2a8d56c4a827e9794d96fe93c5bd32953c3413be
supert111 <sportsmen@bigmir.net>
2021-03-14 00:08:04 +0200

    seting template

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,7 +1,10 @@
 import galleryTemplate from '../templates/gallery-template.hbs';
-
+import cardTemplate from '../templates/card-template.hbs';
+import setting from '../js/settings/index';
 var debounce = require('lodash.debounce');
-const key = '20667930-64a6ab52d11330f7fc72003b0';
+
+const { BASE_URL, API_KEY } = setting;
+const pageNumber = 1;
 
 const inputFormRef = document.querySelector('.search-form');
 const inputName = inputFormRef.elements.query;
@@ -15,9 +18,11 @@ console.log(searchKeyWord);
 // const search
 
 
-async function fetch (searchKey) {
-    const searchResult = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchKey}&page=номер_страницы&per_page=12&key=${key}`)
+async function fetchImage (searchKey) {
+    const searchResult = await fetch(`${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${searchKey}&page=${pageNumber}&per_page=12&key=${API_KEY}`);
+    const result = await searchResult.json();
+    return result;
 }
-
+ console.log(fetchImage('yellow flowers'));
 //https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchKey}&page=номер_страницы&per_page=12&key=твой_ключ
 // key: 20667930-64a6ab52d11330f7fc72003b0
