76a5d126e805175356612ad14d3fb401b8ca8680
supert111 <sportsmen@bigmir.net>
2021-03-14 21:57:06 +0200

    style

--- src/js/images-pagination/index.js
+++ src/js/images-pagination/index.js
@@ -1,17 +1,23 @@
-class indexPagination {
-    constructor(selector, totalPages) {
-        this.element = document.querySelector(selector);
-        this.currentPage = 0;
-        this.totalPages = totalPages;
-    }
+// class indexPagination {
+//     constructor(selector, totalPages) {
+//         this.element = document.querySelector(selector);
+//         this.currentPage = 0;
+//         this.totalPages = totalPages;
+//     }
 
     // goToPrevPage()  {
+    //     if(this.currentPage === 1) {
+    //         return;
+    //     }
     //     this.currentPage -= 1;
     // }
 
-    goToNextPage()  {
-        this.currentPage += 1;
-    }
-}
+//     goToNextPage()  {
+//         if(this.currentPage === this.totalPages) {
+//             return;
+//         }
+//         this.currentPage += 1;
+//     }
+// }
 
-export default indexPagination
+// export default indexPagination
