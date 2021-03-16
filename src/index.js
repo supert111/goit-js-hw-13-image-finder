b920ef2a5f1be2ec5a6dc75c0be169077c08e84b
supert111 <sportsmen@bigmir.net>
2021-03-17 01:32:20 +0200

    patch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './styles.css';
+import '../node_modules/material-design-icons/iconfont/material-icons.css';
 import NewsApiService from './js/apiService';
+import notifaer from './js/notifier'
+
 var debounce = require('lodash.debounce');
+
+
 
 const inputFormRef = document.querySelector('.search-form');
 const inputName = inputFormRef.elements.query;
@@ -12,21 +17,24 @@ const newsApiService = new NewsApiService;
 //прослушиватель на инпут + debounce
 inputName.addEventListener('input', debounce(() =>{
     newsApiService.query = inputName.value;
-        if (newsApiService.query === '') {
-            return alert('Введи что-то нормальное');
-          }
+    if (newsApiService.query === '') {
+        return
+        notifaer.error({text: "Empty field, please enter your request!"});
+    }
     newsApiService.resetPage();
     const searchSee = newsApiService.query;
     newsApiService.fetchArticles(searchSee);
     
-        // fetchImage () {
+    // fetchImage () {
         // }
-            // .then(renderImages)
-           // .catch(error => console.log(error));
-    
-    console.log(newsApiService.query);
+        // .then(renderImages)
+        // .catch(error => console.log(error));
+        
+        console.log(newsApiService.query);
     },1000)); 
-
+    
     function fetchArticles () {
         newsApiService.fetchArticles();
     }
+
+    
---
+++ src/js/notifier.js
@@ -0,0 +1,23 @@
+import { alert, defaultModules, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+//import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+//import { defaults } from '@pnotify/core';
+import '../node_modules@pnotify/core/dist/Material.css';
+
+class Notifier {
+    error(text) {
+        error ({ text });
+    }
+    alert(text) {
+        alert ({ text });
+    }
+}
+
+// defaultModules.set(PNotifyMobile, {});
+
+
+// defaults.styling = 'material';
+// defaults.icons = 'material';
+// defaults.delay = 2000;
+// defaults.error = error;
+
+export default new Notifier ();
